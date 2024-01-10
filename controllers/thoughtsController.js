8de565a2168bfb83afcc77735d32b11f285a2403
Uriel <CarrilloUriel81@gmail.com>
2024-01-10 13:13:22 -0800

    Updated ReactionsController to be a subdocument to thoughtsControllers

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,4 +1,4 @@
-const { Thoughts, User } = require('../models');
+const { User, Thoughts } = require('../models');
 
 const thoughtsController = {
   getAllThoughts(req, res) {
@@ -88,6 +88,42 @@ const thoughtsController = {
       })
       .catch((err) => res.status(500).json(err));
   },
-};
+  
+    createReaction(req, res) {
+      const { thoughtsId } = req.params;
+      const { reactionBody, username } = req.body;
+  
+      Thoughts.findByIdAndUpdate(
+        thoughtsId,
+        { $push: { reactions: { reactionBody, username } } },
+        { new: true }
+      )
+        .then((thoughts) => {
+          if (!thoughts) {
+            return res.status(404).json({ message: 'Thoughts not found.' });
+          }
+          res.json(thoughts);
+        })
+        .catch((err) => res.status(500).json(err));
+    },
+  
+    removeReaction(req, res) {
+      const { thoughtsId, reactionId } = req.params;
+  
+      Thoughts.findByIdAndUpdate(
+        thoughtId,
+        { $pull: { reactions: { _id: reactionId } } },
+        { new: true }
+      )
+        .then((thoughts) => {
+          if (!thoughts) {
+            return res.status(404).json({ message: 'Thoughts not found.' });
+          }
+          res.json(thoughts);
+        })
+        .catch((err) => res.status(500).json(err));
+    },
+  };
+
 
 module.exports = thoughtsController;
